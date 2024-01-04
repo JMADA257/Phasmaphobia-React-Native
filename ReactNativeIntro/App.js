682f78fb9a75007f7e046f2b70083667ec44e32a
JMADA257 <jadams1812@hotmail.com>
2024-01-04 14:18:19 -0700

    Started the home page of the app

--- ReactNativeIntro/App.js
+++ ReactNativeIntro/App.js
@@ -1,7 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const handlePressButtonGadgets = () => {
+    console.log("Gadgets Button pressed!");
+  };
+  const handlePressButtonMaps = () => {
+    console.log("Maps Button pressed!");
+  };
+  const handlePressButtonUnlockables = () => {
+    console.log("Unlockable Button pressed!");
+  };
+  const handlePressButtonGhosts = () => {
+    console.log("Ghost Button pressed!");
+  };
+  const handlePressButtonContact = () => {
+    console.log("Contact Button pressed!");
+  };
+
   return (
     <>
       <SafeAreaView style={styles.container}>
@@ -10,13 +34,41 @@ export default function App() {
           This App is to help guide others through the great game of
           Phasmaphobia
         </Text>
+
         <StatusBar style="auto" />
+
+        <TouchableOpacity
+          style={styles.Button}
+          onPress={handlePressButtonGadgets}
+        >
+          <Text style={styles.buttonText}>Gadgets</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.Button} onPress={handlePressButtonMaps}>
+          <Text style={styles.buttonText}>Maps</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.Button}
+          onPress={handlePressButtonUnlockables}
+        >
+          <Text style={styles.buttonText}>Unlockables</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.Button}
+          onPress={handlePressButtonGhosts}
+        >
+          <Text style={styles.buttonText}>Ghosts</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.Button}
+          onPress={handlePressButtonContact}
+        >
+          <Text style={styles.buttonText}>Contact / Donations</Text>
+        </TouchableOpacity>
       </SafeAreaView>
-      <View>
-        <Image source={
-          
-          require("./assets/icon.png")} />
-      </View>
     </>
   );
 }
@@ -26,5 +78,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    textAlign: "center",
+  },
+  Button: {
+    minWidth: "85%",
+    minHeight: "10%",
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 15,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+    margin: "auto",
   },
 });
